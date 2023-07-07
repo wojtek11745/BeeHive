a8a074948857d9d1df5d31f4fdeb953fad842b1b
wojtek <wojtej1174@gmail.com>
2023-07-07 16:51:09 +0200

    Blank project

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+//blank project
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
